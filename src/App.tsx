e8a3b579af5fcc7f01145cce26f03bee79378e7b
Dmedaura <126912879+Dmedaura@users.noreply.github.com>
2024-12-02 18:36:26 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import viteLogo from "/vite.svg";
 import { IPersona } from "./types/IPersona";
 import PersonaForm from "./Components/PersonaForm";
 import { ITareas } from "./types/ITareas";
+import Tareas from "./Components/Tareas";
+import { POST } from "./services/Peticiones";
 
 function App() {
 
@@ -19,13 +21,45 @@ function App() {
     traerTareas()
   }, [])
 
-  console.log(tareas)
-  
+  const crearTarea = async () => {
+    const nuevaTarea: ITareas = {
+      nombre: "Aprender TypeScript",
+      prioridad: "Alta",
+      finalizada: false,
+    };
+
+    try {
+      const respuesta = await POST<ITareas>('http://localhost:3000/tareas', nuevaTarea);
+      console.log('Tarea creada:', respuesta);
+    } catch (error) {
+      console.error('Error creando la tarea:', error);
+    }
+  };
+
+  //console.log(tareas)
+
   return (
 
-    
+
     <>
-      
+      <div>
+        <form>
+          <input type="text" placeholder="Nombre de la tarea" required />
+          <button type="submit" >Agregar Tarea</button>
+        </form>
+      </div>
+      <div><button onClick={crearTarea}>Crear Tarea</button></div>
+      <div>
+        {tareas.map((tarea) => (
+          <div key={tarea.id}> {/* Usa un identificador único como `key` */}
+            <p>Nombre: {tarea.nombre}</p>
+            <p>Prioridad: {tarea.prioridad}</p>
+            <p>Finalizada: {tarea.finalizada ? "Sí" : "No"}</p>
+          </div>
+        ))}
+      </div>
+
+
     </>
   );
 }
--- src/Components/Tareas.tsx
+++ src/Components/Tareas.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react'
 import { ITareas } from '../types/ITareas'
 
-const Tareas: FC<ITareas> = ({id,finalizada,nombre,prioridad}) => {
+const Tareas: FC<ITareas> = ({finalizada,nombre,prioridad}) => {
   return (
     <div>
         <h2>{nombre}</h2>
         <h3>{prioridad}</h3>
-        <h4></h4>
+        <h4>{finalizada}</h4>
 
     </div>
   )
